711ecc08c303173525d94d9ad8a14b0e19a964bc
seiya1911 <n.siy1911cb@gmail.com>
2021-05-11 09:23:32 +0000

    [Update]各view

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -25,6 +25,16 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to books_path
+  end
+
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
-  resources :books, only: [:new, :create, :index, :show, :destroy]
+  resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update]
 
   resources :users, only: [:new, :create, :show, :edit, :update, :index]
 
